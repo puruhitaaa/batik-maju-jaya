56fbe78c34ab1eb13e7366cfd040feb19fcb7583
puruhitaaa <hughdev101@gmail.com>
2022-04-07 17:11:48 +0700

    [+-] Important update

--- src/constants/url.ts
+++ src/constants/url.ts
@@ -1,4 +1,4 @@
-export const BASE_URL = process.env.API_URI;
+export const BASE_URL = process.env.REACT_APP_API_URI;
 
 export const USER_LOGIN_PATH = `${BASE_URL}/api/users/login`;
 export const USERS_PATH = `${BASE_URL}/api/users`;
